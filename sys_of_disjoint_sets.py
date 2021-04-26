2a6ee3bcdd93df70099f112fae58f47a5464958c
Anastasia P <petrichuk.nastya@yandex.ru>
2021-04-26 20:57:28 +0300

    добавлена задача об объединении таблиц

---
+++ sys_of_disjoint_sets.py
@@ -0,0 +1,41 @@
+_max = 0
+
+
+def make_set(i, parent, rank):
+    parent[i] = i
+    rank[i] = 0
+
+
+def find(i, parent):
+    if i != parent[i]:
+        parent[i] = find(parent[i], parent)
+    return parent[i]
+
+
+def union(dest, source, parent, rank):
+    global _max
+    d_parent = find(dest, parent)
+    s_parent = find(source, parent)
+
+    if d_parent != s_parent:
+        parent[s_parent] = d_parent
+        rank[d_parent] += rank[s_parent]
+        _max = max(_max, rank[d_parent])
+    return _max
+
+
+def main():
+    global _max
+
+    n, m = map(int, input().split())    # число таблиц, число запросов
+    parent = [i for i in range(n)]
+    rank = list(map(int, input().split()))
+    _max = max(rank)
+
+    for _ in range(m):
+        dest, source = map(int, input().split())
+        print(union(dest - 1, source - 1, parent, rank))
+
+
+if __name__ == '__main__':
+    main()
